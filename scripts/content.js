68808d0c4cffb50953bb796cb645e2bb314e492b
EKLEMIS SANTO NDUN <eklemis.ndun@gmail.com>
2023-03-06 14:30:39 +0800

    can catch selected element and the selected text itself

--- scripts/content.js
+++ scripts/content.js
@@ -1,16 +1,22 @@
+const textSelectedHandler = (ev) => {
+	let selectedText = "";
+	if (window.getSelection()) {
+		selectedText = window.getSelection().toString();
+		console.log("selected el:", ev.target);
+		console.log("location:", window.location.href);
+		console.log("Selected text:", selectedText);
+	}
+};
+
 const allParagraphs = document.querySelectorAll("p");
 const allCodes = document.querySelectorAll("code");
-const onSelect = (ev) => {
-	console.log("selected el:", ev.target.innerText);
-	console.log("location:", window.location.href);
-};
 
 allParagraphs.forEach((paragh) => {
 	//paragh.style = "background-color: yellow;";
-	paragh.addEventListener("click", onSelect);
+	paragh.addEventListener("click", textSelectedHandler);
 });
 
 allCodes.forEach((code) => {
 	//code.style = "background-color: yellow;";
-	code.addEventListener("select", onSelect);
+	code.addEventListener("select", textSelectedHandler);
 });
