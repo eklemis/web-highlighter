054de5c57baa835515b12d7ac047e18e443a8eb2
EKLEMIS SANTO NDUN <eklemis.ndun@gmail.com>
2023-03-06 08:55:09 +0800

    try p select event

--- scripts/content.js
+++ scripts/content.js
@@ -1,22 +1,16 @@
-const article = document.querySelector("article");
+const allParagraphs = document.querySelectorAll("p");
+const allCodes = document.querySelectorAll("code");
+const onSelect = (ev) => {
+	console.log("selected el:", ev.target.innerText);
+	console.log("location:", window.location.href);
+};
 
-// `document.querySelector` may return null if the selector doesn't match anything.
-if (article) {
-	const text = article.textContent;
-	const wordMatchRegExp = /[^\s]+/g; // Regular expression
-	const words = text.matchAll(wordMatchRegExp);
-	// matchAll returns an iterator, convert to array to get word count
-	const wordCount = [...words].length;
-	const readingTime = Math.round(wordCount / 200);
-	const badge = document.createElement("p");
-	// Use the same styling as the publish information in an article's header
-	badge.classList.add("color-secondary-text", "type--caption");
-	badge.textContent = `⏱️ ${readingTime} min read`;
+allParagraphs.forEach((paragh) => {
+	//paragh.style = "background-color: yellow;";
+	paragh.addEventListener("click", onSelect);
+});
 
-	// Support for API reference docs
-	const heading = article.querySelector("h1");
-	// Support for article docs with date
-	const date = article.querySelector("time")?.parentNode;
-
-	(date ?? heading).insertAdjacentElement("afterend", badge);
-}
+allCodes.forEach((code) => {
+	//code.style = "background-color: yellow;";
+	code.addEventListener("select", onSelect);
+});
